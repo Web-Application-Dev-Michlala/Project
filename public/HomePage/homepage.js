97e4eac926049134c1f6aa6581ea1cbf91e22d42
Ziv Siag <ziv.siag@gmail.com>
2023-06-20 17:39:11 +0300

    Added Footer

--- public/HomePage/homepage.js
+++ public/HomePage/homepage.js
@@ -1,141 +1,41 @@
-// $(function()
-// {
-    
-// $.ajax
-//     ({
-//         url:'/getCategorys',
-//     }).done(function(data)
-//         {
-//             var counter=0;
-//             var catCounter=0;
-//             data.forEach(Category => 
-//             {   
-//                 if(catCounter%3==0)
-//                 {
-//                     $('body').append('<div class="row justify-content-md-center" id="row+catCounter">')
-//                 }
-//                 var rowID=Math.floor(catCounter/3);
-//                 $('#row+rowID').append('<div onclick="window.location.href="#" class="card text-center col-2" style="width: 18rem;"> <div class="card-body"><h5 class="card-title">'+Category.categoryName+'</h5></div></div>')
-//                 catCounter++;
-//                 const Hotarray=filterByHot(Category.products);
-//                 for(let i=0;i<Hotarray.length;i++)
-//                 { 
-//                     if(counter==0)
-//                     {
-//                         $('#caroursel-ind').append('<button type="button" data-bs-target="#mycar" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide+(counter+1)" ></button>')
-//                         $('#carousel').append('<div class:"carousel-item active c-item"> <a href="Hotarray[i].link><img src="Hotarray[i].IMG" class="d-block w-100 c-img" ></div></a><div class="carousel-caption d-none d-md-block"><h5>+Hotarray[i].Name</h5></div>')
-//                     }
-//                     else
-//                     {
-//                         $('#caroursel-ind').append('<button type="button" data-bs-target="#mycar" data-bs-slide-to="counter" aria-label="Slide+(counter+1)"></button>')
-//                         $('#carousel').append('<div class:"carousel-item c-item"> <a href="Hotarray[i].link><img src="Hotarray[i].IMG" class="d-block w-100 c-img" ></div></a><div class="carousel-caption d-none d-md-block"><h5>Hotarray[i].Name</h5></div>')
-//                     }
-//                     counter++
-               
-//                 }
-//             });
-            
-//         });
-// });
 
-// function filterByHot(products)
-// {
-//     prod=products;
-//     for(let i=0;i<prod.length;i++)
-//     {
-//         if(!prod[i].hot)
-//         prod[i].remove();
-//     }
-//     return prod
-// }
 $( document ).ready(function() 
 {
-const arr=[];
+carouselnum=1;
+var arr=[];
     $.ajax
         ({
             url:'/Phones',
           
         }).done(function(data)
             {
-                data.forEach(product => {
+                data.products.forEach(product => {
                     if(product.hot)
                     arr.push(product)
                 });
-                createAndloadHotCarouselsFromDB(arr,1)
+                const length = arr.length;
+                const partSize = Math.floor(length / 3);
+                
+                const part1 = arr.slice(0, partSize);
+                const part2 = arr.slice(partSize, partSize * 2);
+                const part3 = arr.slice(partSize * 2);
+                createAndloadHotCarouselsFromDB(part1,carouselnum)
+                carouselnum++;
+                createAndloadHotCarouselsFromDB(part2,carouselnum)
+                carouselnum++;
+                createAndloadHotCarouselsFromDB(part3,carouselnum)
             });
-    donfil=1;
-    bonfil=2;
-    bon=1;
-    don=1;
-    gon=1;
+   
+            bon=1
+            don=1
+            gon=1
+            bonfil=1
+            donfil=1;
     const categoryArray=[bon,don,gon,bonfil,donfil]
-    const hotArray=[donfil,bonfil,don]
-    //createAndloadHotCarousels(hotArray,1);
-   // createAndloadHotCarousels(hotArray,2);
-    //createAndloadHotCarousels(hotArray,3)
+    
+   
     loadCategories(categoryArray);
-    //
-    console.log(arr.length);
-function createAndloadHotCarousels(hotItems, carouselnum)//loading the carousels with items 
-{
-
-    var carousel=document.createElement('div');
-    carousel.classList.add("carousel")
-    carousel.classList.add("slide")
-    carousel.classList.add("c-slide")
-    carousel.classList.add("container")
-
-    carousel.setAttribute("data-bs-ride", "carousel");
-    carousel.setAttribute("id","carousel"+(carouselnum));
-    var rowcon=document.getElementById('rowcon');
-    rowcon.appendChild(carousel);
-    var data;
-    var caritem;
-    for(let i=0;i<hotItems.length;i++)
-    {
-        if( i===0)
-        {
-            data='<button type="button" data-bs-target="#carousel'+carouselnum+'" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>'
-            caritem='<div class="carousel-item active c-item">'+
-            '<a href="file:///D:/College/Year_2_SemesterB/Application%20Dev/Project%20components/Homepage/Homepage.html">'+//<!--TODO PULL hot product IMAGE AND LINK-->
-            '<img src="https://images.pexels.com/photos/14918477/pexels-photo-14918477.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="d-block  c-img" >'+
-            '</a>'+
-            '<div class="carousel-caption d-none d-md-block" id="title">'+
-                '<h5>First prodcut name</h5>'+//<!--TODO PULL hot product name-->
-            '</div>'+
-          '</div>';
-        }
-        else
-        {
-            data+='<button type="button" data-bs-target="#carousel'+carouselnum+'" data-bs-slide-to='+(i)+' aria-label="Slide '+(i+1)+'"></button>'
-            caritem+='<div class="carousel-item c-item">'+
-            '<a href="file:///D:/College/Year_2_SemesterB/Application%20Dev/Project%20components/Homepage/Homepage.html">'+//<!--TODO PULL hot product IMAGE AND LINK-->
-            '<img src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGljfGVufDB8fDB8fHww&w=1000&q=80" class="d-block  c-img">'+
-            '</a>'+
-            '<div class="carousel-caption d-none d-md-block" id="title">'+
-                '<h5>Second prodcut name</h5>'+//<!--TODO PULL hot product name-->
-            '</div>'+
-          '</div>';
-        }
-    }
-    carousel.innerHTML=
-    '<div class="carousel-inner" id="carousel">'+
-    '<div class="carousel-indicators" id="caroursel-ind">'+
-    data+
-    '</div>'+
-    '<div class="carousel-inner" id="carousel">'+
-    caritem+
-    '</div>'+
-    '<button class="carousel-control-prev" type="button" data-bs-target="#carousel'+carouselnum+'"data-bs-slide="prev">'+
-    '<span class="carousel-control-prev-icon" aria-hidden="true"></span>'+
-    '<span class="visually-hidden">Previous</span>'+
-    '</button>'+
-    '<button class="carousel-control-next" type="button" data-bs-target="#carousel'+carouselnum+'" data-bs-slide="next">'+
-    '<span class="carousel-control-next-icon" aria-hidden="true"></span>'+
-    '<span class="visually-hidden">Next</span>'+
-    '</button>'+
-    '</div>';
-}
+  
 function createAndloadHotCarouselsFromDB(hotItems, carouselnum)//loading the carousels with items 
 {
 
@@ -158,10 +58,10 @@ function createAndloadHotCarouselsFromDB(hotItems, carouselnum)//loading the car
             data='<button type="button" data-bs-target="#carousel'+carouselnum+'" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>'
             caritem='<div class="carousel-item active c-item">'+
             '<a href="file:///D:/College/Year_2_SemesterB/Application%20Dev/Project%20components/Homepage/Homepage.html">'+//<!--TODO PULL hot product IMAGE AND LINK-->
-            '<img src="https://images.pexels.com/photos/14918477/pexels-photo-14918477.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="d-block  c-img" >'+
+            '<img src="./'+hotItems[i].image+'" class="d-block  c-img" >'+
             '</a>'+
             '<div class="carousel-caption d-none d-md-block" id="title">'+
-                '<h5>'+"hotItems[i]"+'</h5>'+//<!--TODO PULL hot product name-->
+                '<h5>'+hotItems[i].name+'</h5>'+//<!--TODO PULL hot product name-->
             '</div>'+
           '</div>';
         }
@@ -170,10 +70,10 @@ function createAndloadHotCarouselsFromDB(hotItems, carouselnum)//loading the car
             data+='<button type="button" data-bs-target="#carousel'+carouselnum+'" data-bs-slide-to='+(i)+' aria-label="Slide '+(i+1)+'"></button>'
             caritem+='<div class="carousel-item c-item">'+
             '<a href="file:///D:/College/Year_2_SemesterB/Application%20Dev/Project%20components/Homepage/Homepage.html">'+//<!--TODO PULL hot product IMAGE AND LINK-->
-            '<img src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGljfGVufDB8fDB8fHww&w=1000&q=80" class="d-block  c-img">'+
+            '<img src="./'+hotItems[i].image+'" class="d-block  c-img">'+
             '</a>'+
             '<div class="carousel-caption d-none d-md-block" id="title">'+
-                '<h5>'+"dads"+'</h5>'+//<!--TODO PULL hot product name-->
+                '<h5>'+hotItems[i].name+'</h5>'+//<!--TODO PULL hot product name-->
             '</div>'+
           '</div>';
         }
@@ -195,6 +95,7 @@ function createAndloadHotCarouselsFromDB(hotItems, carouselnum)//loading the car
     '<span class="visually-hidden">Next</span>'+
     '</button>'+
     '</div>';
+    
 }
 function loadCategories(categoryArray)
 {
@@ -210,7 +111,8 @@ function loadCategories(categoryArray)
     cardRow.classList.add('row');
     cardRow.classList.add('justify-content-md-center');
     cardRow.classList.add('cards-row');
-    document.body.appendChild(cardRow);
+    container=document.getElementById('categories');
+    container.appendChild(cardRow);
         card1=
         "<div onclick=\"window.location.href='file:///D:/College/Year_2_SemesterB/Application%20Dev/Project%20components/Homepage/Homepage.html'\" class=\"card text-center flex-fill\" style=\"width: 18rem;\">"+
 
--- routes/homepageRoute.js
+++ routes/homepageRoute.js
@@ -7,7 +7,19 @@ router.route('/')
     .get(homepageController.getHomePage)
 
 
-
+    router.route('/ReturnPolicy').get((req, res) => {
+        res.sendFile('returnPolicy.html', { root: 'public/ReturnPolicy' });
+      });
+      router.route('/PrivacyPolicy').get((req, res) => {
+        res.sendFile('privacyPolicy.html', { root: 'public/PrivacyPolicy' });
+      });
+      router.route('/QuestionsAndAnswers').get((req, res) => {
+        res.sendFile('FAQ.html', { root: 'public/FAQ' });
+      });
+      router.route('/About').get((req, res) => {
+        res.sendFile('returnPolicy.html', { root: 'public/About' });
+      });
+    
 router.route('/getCategorys')
     .get(homepageController.getAllCategorys)
 
