d647cf83763f87c2081a8363f7540c05533eb293
Shahar Gutman <shahargut1@gmail.com>
2023-06-20 12:32:22 +0300

    Pull from github

--- loadTodb.js
+++ loadTodb.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const category = require('./services/category')
-const product = require('./services/product')
+
+
 try{
     mongoose.connect('mongodb+srv://shahargut2:eMNpX8KBONU5H324@cluster0.lctj22i.mongodb.net/');
     console.log('Connected to MongoDB');
     loadDB();
-   
+    
     
 
 
@@ -13,12 +14,25 @@ try{
     console.error('Error connecting to MongoDB', err);
 }
 
-async function loadDB(){
+
+
+function loadDB(){
     console.log('insert to db')
-    const products = await product.createProduct('Iphone',100,'Phones','Red',14.2,'public/images/Phones.jpg','This is a phone',1000,5,"Apple",false,'Some more text');
-
-    await category.createCategory('Phones');
-
-    await category.addProductToCategory('Phones',products);
+    category.createCategory('Phones');
+    
+    /*
+    const prodArray= getAllProducts();
+    for(const product of prodArray)
+    {
+        category.addProductToCategory('Phones',product);
+    }
+    //const products = await product.createProduct('Iphone',100,'Phones','Red',14.2,'public/images/Phones.jpg','This is a phone',1000,5,"Apple",false,'Some more text');
+    //await category.addProductToCategory('Phones',products);
+    */
     console.log('done')
+}
+function getAllProducts() {
+    
+    const products = modelproduct.find();
+    return products;
 }
